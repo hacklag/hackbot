0033f479f1de05ff48172383fc6bdbb3f358d8c1
kkkowalski <karol.kowalski@eyedea.io>
2016-04-09 17:22:45 +0200

    Changed deploy function & circle tests

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-scp -r * $BOT_USERNAME@$BOT_HOST:/home/$BOT_USERNAME/hacklag-bot/
+rsync plugins/* $BOT_USERNAME@$BOT_HOST:/home/$BOT_USERNAME/hacklag-bot/plugins
+rsync -p --chmod=+rwx rtmbot.py -z $BOT_USERNAME@$BOT_HOST:/home/$BOT_USERNAME/hacklag-bot/
+rsync -p --chmod=+rwx startup.sh -z $BOT_USERNAME@$BOT_HOST:/home/$BOT_USERNAME/hacklag-bot/
