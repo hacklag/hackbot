ccd3a92dddec3b70668ab79fc06a7ff3d61c068b
kkkowalski <karol.kowalski@eyedea.io>
2016-04-13 15:42:34 +0200

    Added simple message attachments with colors to lottery

--- plugins/lottery.py
+++ plugins/lottery.py
@@ -8,12 +8,23 @@ def process_message(data, ctx):
   channel = data["channel"]
   text = data["text"]
 
+  generated_number = randint(1, 10)
+  generated_color = "#ff0000"
+
+  if 4 <= generated_number < 7:
+    generated_color = "#ff7700"
+  elif 7 <= generated_number < 11:
+    generated_color = "#00ff00"
+
+  attach = [{"color": generated_color, "title": "Generating number from range 1 to 10... ","text": "Number: "+str(generated_number)}]
+
   if text.startswith("<@U0Q74DWT1>"):
     if "lottery" in text:
       ctx.api_call(
           "chat.postMessage",
           channel=data["channel"],
-          text="Lottery initiated by <@"+data['user']+"> \n Generating number from range 1 to 10... \n Number is: `"+str(randint(1, 10))+"`",
+          text="Lottery initiated by <@"+data['user']+">",
           link_names=1,
-          as_user="true"
+          as_user="true",
+          attachments=json.dumps(attach)
       )
